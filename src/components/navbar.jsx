b80d3885fe4ca25cc107c382d5ec7a65c7c33032
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2024-04-22 16:40:50 +0200

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,139 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import NavLink from "./navLink";
+import { motion, stagger } from "framer-motion";
+
+const links = [
+    { title: "Home", url: "/" },
+    { title: "About", url: "/about" },
+    { title: "Portfolio", url: "/portfolio" },
+    { title: "Contact", url: "/contact" },
+];
+const socials = [
+    { title: "github", url: "https://www.github.com/kandapagari" },
+    { title: "linkedin", url: "https://www.linkedin.com/in/kandapagari" },
+    { title: "twitter", url: "https://www.twitter.com/zephyr_347" },
+    { title: "instagram", url: "https://www.instagram.com/abhimanyu_pavan" },
+    { title: "facebook", url: "https://www.facebook.com/abhi619pavan" },
+];
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const topVariants = {
+        closed: { rotate: 0 },
+        opened: {
+            rotate: 45,
+            backgroundColor: "rgb(255,255,255)"
+        },
+    };
+
+    const middleVariants = {
+        closed: { opacity: 1 },
+        opened: {
+            opacity: 0,
+        },
+    };
+
+    const bottomVariants = {
+        closed: { rotate: 0 },
+        opened: {
+            rotate: -45,
+            backgroundColor: "rgb(255,255,255)"
+        },
+    };
+
+    const listVariants = {
+        closed: { x: "100vw" },
+        opened: {
+            x: 0,
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.2
+            },
+        }
+    };
+
+    const listItemVariants = {
+        closed: {
+            x: -10,
+            opacity: 0
+        },
+        opened: {
+            x: 0,
+            opacity: 1
+        }
+    }
+    return (
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+            {/* LINKS */}
+            <div className="hidden md:flex gap-4 w-1/3">
+                {links.map(link => (
+                    <NavLink link={link} key={link.title} />
+                ))}
+            </div>
+            {/* LOGO */}
+            <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+                <Link
+                    href="/"
+                    className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+                >
+                    <span className="text-white mr-1">
+                        Kandapagari
+                    </span>
+                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+                        .ai
+                    </span>
+                </Link>
+            </div>
+            {/* SOCIALS */}
+            <div className="hidden md:flex gap-4 w-1/3">
+                {socials.map(social => (
+                    <Link href={social.url} key={social.title} target="_blank">
+                        <Image src={`/${social.title}.png`} alt={social.title} width={24} height={24} />
+                    </Link>
+                ))}
+            </div>
+            {/* RESPONSIVE MENU */}
+            <div className="md:hidden">
+                {/* MENU BUTTON */}
+                <button
+                    className="w-10 h-8 flex flex-col justify-between z-50 relative"
+                    onClick={(() => setOpen(prev => !prev))}>
+                    <motion.div
+                        variants={topVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left"></motion.div>
+                    <motion.div
+                        variants={middleVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded"></motion.div>
+                    <motion.div
+                        variants={bottomVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left"></motion.div>
+                </button>
+                {/* MENU LIST */}
+                {open && (
+                    <motion.div
+                        variants={listVariants}
+                        initial="closed"
+                        animate="opened"
+                        className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
+                        {links.map(link => (
+                            <motion.div
+                                variants={listItemVariants}
+                                key={link.title}>
+                                <Link href={link.url}> {link.title} </Link>
+                            </motion.div>
+
+                        ))}
+                    </motion.div>)}
+            </div>
+        </div >
+    );
+}
+
+export default Navbar;
